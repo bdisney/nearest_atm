c0afeb5614add4ae280b4aceaca557ca29bba99a
disne_y <bold.disney@gmail.com>
2018-05-06 19:58:55 +0300

    Cache sql query by lat, long keys

--- app/models/atm.rb
+++ app/models/atm.rb
@@ -6,15 +6,17 @@ class Atm < ApplicationRecord
   DISTANCE_THRESHOLD = 25000.freeze
 
   def self.nearest(lat, long, distance_threshold = DISTANCE_THRESHOLD)
-    Location.find_by_sql("
-      SELECT * FROM (SELECT *, ( 6371 * acos( cos( radians(#{lat}) ) * cos( radians( latitude ) )
-      * cos( radians( longitude ) - radians(#{long}) )
-      + sin( radians(#{lat}) ) * sin ( radians ( latitude ) ) ) ) AS distance
-      FROM locations
-      LEFT OUTER JOIN atms on locations.atm_id = atms.id) as qq
-      WHERE distance < #{distance_threshold}
-      ORDER BY distance
-      LIMIT 5;
-    ")
+    Rails.cache.fetch([self.class.name, lat, long], expires_in: 1.hour) {
+      Location.find_by_sql("
+        SELECT * FROM (SELECT *, ( 6371 * acos( cos( radians(#{lat}) ) * cos( radians( latitude ) )
+        * cos( radians( longitude ) - radians(#{long}) )
+        + sin( radians(#{lat}) ) * sin ( radians ( latitude ) ) ) ) AS distance
+        FROM locations
+        LEFT OUTER JOIN atms on locations.atm_id = atms.id) as qq
+        WHERE distance < #{distance_threshold}
+        ORDER BY distance
+        LIMIT 5;
+      ")
+    }
   end
 end

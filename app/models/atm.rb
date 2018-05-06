91450b8e03af49d3f1e1c08170efdc45b0ff6b53
disne_y <bold.disney@gmail.com>
2018-05-06 19:58:02 +0300

    Add method which returns nearest locations with distance and atm_id

--- app/models/atm.rb
+++ app/models/atm.rb
@@ -2,4 +2,19 @@ class Atm < ApplicationRecord
   has_one :location, dependent: :destroy
 
   validates_associated :location
+
+  DISTANCE_THRESHOLD = 25000.freeze
+
+  def self.nearest(lat, long, distance_threshold = DISTANCE_THRESHOLD)
+    Location.find_by_sql("
+      SELECT * FROM (SELECT *, ( 6371 * acos( cos( radians(#{lat}) ) * cos( radians( latitude ) )
+      * cos( radians( longitude ) - radians(#{long}) )
+      + sin( radians(#{lat}) ) * sin ( radians ( latitude ) ) ) ) AS distance
+      FROM locations
+      LEFT OUTER JOIN atms on locations.atm_id = atms.id) as qq
+      WHERE distance < #{distance_threshold}
+      ORDER BY distance
+      LIMIT 5;
+    ")
+  end
 end
